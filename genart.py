75a4d28029f27311d8490116245cff131c1edcb4
YachtRocker <douglasjkohn@gmail.com>
2021-10-07 11:27:53 -0500

    remove pixels and create a new image

--- genart.py
+++ genart.py
@@ -3,7 +3,6 @@ import numpy as np
 
 #creates a random pixel picture
 def random_img(output, width, height):
-
     #array = np.random.random_integers(0,255, (height,width,3))  
     array = np.random.randint(0,255, (height,width,3))
     array = np.array(array, dtype=np.uint8)
@@ -19,15 +18,26 @@ def pixel_info(input):
     pixel_values = list(img.getdata())
     newPixValues = np.array(pixel_values)  #converts list to array
 
-    #return pixel_values
-    return newPixValues
+    return pixel_values
+    #return newPixValues
 
 #create a new image just using a range from a previous image
 def create_image_range(pix_values):
+    new_img_array = []
     for rgb in pix_values:
-        if rbg[0] > 200 and rgb[0] < 230:
-            print(rgb)
+        if rgb[0] > 200 and rgb[0] < 230:
+            new_img_array.append(rgb)
 
-#random_img('random3.png', 50, 150)
+    array = np.array(new_img_array, dtype=np.uint8)
+    print(array)
+    img = Image.fromarray(array)
+    img.save("./new_image.png")  
+
+
+
+#random_img('./random.png', 50, 150)
 image = pixel_info("random.png")
-print(image)
+create_image_range(image)
+
+
+
